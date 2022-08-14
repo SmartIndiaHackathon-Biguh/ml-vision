62ec83f5ecdf689a19b910db0ff3d405dcf64a48
Dhruv Dange <dhruvdange@gmail.com>
2022-08-14 15:01:54 +0530

    Fixed bugs and image loads properly

--- lib/Screens/face_detect_screen.dart
+++ lib/Screens/face_detect_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +8,7 @@ import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sih_login/Models/user_model.dart';
 import 'dart:ui' as ui;
+import 'package:sih_login/Modules/FaceDetection/FacePainter.dart';
 // ignore_for_file: prefer_const_constructors
 
 class FaceDetectScreen extends StatefulWidget {
@@ -21,15 +19,14 @@ class FaceDetectScreen extends StatefulWidget {
 }
 
 class _FaceDetectScreenState extends State<FaceDetectScreen> {
-  VoidCallback? analyseFunc = null;
   bool isAnalyse = false;
-
-  File? image;
 
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
 
-  // Face detector options
+  // Face detection values
+  File? image;
+  bool boundedImage = false;
   File? _imageFile;
   List<Face>? _faces;
   bool isLoading = false;
@@ -46,8 +43,9 @@ class _FaceDetectScreenState extends State<FaceDetectScreen> {
         color: Colors.blueAccent,
         borderRadius: BorderRadius.circular(30),
         child: MaterialButton(
+          height: 50,
           padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          minWidth: MediaQuery.of(context).size.width * 0.65,
+          minWidth: MediaQuery.of(context).size.width * 0.55,
           onPressed: onClick,
           child: Text(
             title,
@@ -60,12 +58,17 @@ class _FaceDetectScreenState extends State<FaceDetectScreen> {
 
   Widget analyseButton() => Material(
         elevation: 5,
-        color: isAnalyse ? Colors.amberAccent : Colors.grey,
+        color: isAnalyse ? Colors.indigo.shade900 : Colors.grey,
         borderRadius: BorderRadius.circular(30),
         child: MaterialButton(
+          height: 50,
           padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          minWidth: MediaQuery.of(context).size.width,
-          onPressed: analyseFunc,
+          minWidth: MediaQuery.of(context).size.width * 0.75,
+          onPressed: image == null
+              ? () {
+                  Fluttertoast.showToast(msg: "Please Select an Image");
+                }
+              : faceDetectionFunction,
           child: Text(
             "Analyse Image",
             textAlign: TextAlign.center,
@@ -86,8 +89,8 @@ class _FaceDetectScreenState extends State<FaceDetectScreen> {
         : selectedImage();
   }
 
-Widget selectedImage() {
-    return (imageFile == null)
+  Widget selectedImage() {
+    return !boundedImage
         ? Image.file(
             image!,
             width: MediaQuery.of(context).size.width * 0.70,
@@ -167,14 +170,16 @@ Widget selectedImage() {
 
   Future pickImage(ImageSource source) async {
     try {
+      isLoading = true;
       final image = await ImagePicker().pickImage(source: source);
       if (image == null) return;
 
       final temporaryImage = File(image.path);
       setState(() {
         this.image = temporaryImage;
-        this.analyseFunc = faceDetectionFunction;
+        boundedImage = false;
         isAnalyse = true;
+        isLoading = false;
       });
     } on PlatformException catch (e) {
       Fluttertoast.showToast(msg: e.message.toString());
@@ -195,49 +200,16 @@ Widget selectedImage() {
             _image = value;
             _faces = faces;
             imageFile = image;
+            boundedImage = true;
           }));
 
       if (faces.isNotEmpty) {
-        Fluttertoast.showToast(msg: faces[0].toString());
+        Fluttertoast.showToast(msg: "Face Detected!");
       } else {
         Fluttertoast.showToast(msg: "No face found");
       }
     } catch (e) {
       Fluttertoast.showToast(msg: e.toString());
     }
-  }
-
-  Future drawBoundingBox() async {}
-}
-
-// paint the face
-class FacePainter extends CustomPainter {
-  final ui.Image image;
-  final List<Face> faces;
-  final List<Rect> rects = [];
-
-  FacePainter(this.image, this.faces) {
-    for (var i = 0; i < faces.length; i++) {
-      rects.add(faces[i].boundingBox);
-    }
-    
-  }
-
-  @override
-  void paint(ui.Canvas canvas, ui.Size size) {
-    final Paint paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10.0
-      ..color = Colors.red;
-
-    canvas.drawImage(image, Offset.zero, Paint());
-    for (var i = 0; i < faces.length; i++) {
-      canvas.drawRect(rects[i], paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(FacePainter old) {
-    return image != old.image || faces != old.faces;
   }
 }
