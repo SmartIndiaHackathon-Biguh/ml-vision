ab9a309ad9b7d362f060379d2fc098af486049d2
Dhruv Dange <dhruvdange@gmail.com>
2022-08-25 14:35:58 +0530

    x

--- lib/Screens/child_info_screen.dart
+++ lib/Screens/child_info_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sih_login/Models/user_model.dart';
 import 'package:sih_login/Screens/login_screen.dart';
+import 'package:video_player/video_player.dart';
 // ignore_for_file: prefer_const_constructors
 
 class ChildInfoScreen extends StatefulWidget {
@@ -36,14 +37,38 @@ class _ChildInfoScreenState extends State<ChildInfoScreen> {
   Duration duration = Duration.zero;
   Duration position = Duration.zero;
 
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
   @override
   void initState() {
     super.initState();
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Child Information"),
         centerTitle: true,
@@ -57,7 +82,7 @@ class _ChildInfoScreenState extends State<ChildInfoScreen> {
           // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
             SizedBox(
-                height: MediaQuery.of(context).size.height * 0.40,
+                height: MediaQuery.of(context).size.height * 0.25,
                 child: Image.network(widget.childImage)),
             Text(
               widget.childName,
@@ -99,7 +124,46 @@ class _ChildInfoScreenState extends State<ChildInfoScreen> {
             SizedBox(
               height: 20,
             ),
-            Text("Play Video from Guardian:")
+            Text("Play Video from Guardian:"),
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                // Wrap the play or pause in a call to `setState`. This ensures the
+                // correct icon is shown.
+                setState(() {
+                  // If the video is playing, pause it.
+                  if (_controller.value.isPlaying) {
+                    _controller.pause();
+                  } else {
+                    // If the video is paused, play it.
+                    _controller.play();
+                  }
+                });
+              },
+              // Display the correct icon depending on the state of the player.
+              child: Icon(
+                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+              ),
+            ),
           ],
         ),
       )),
